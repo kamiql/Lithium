df0e7851ca689f0a31ec8144e9ddb6088bb59230
kradtke <kilian.radtke@adesso.de>
2026-07-29 23:26:14 +0200

    Initial project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "Lithium"
+
+include("common")
+include("minecraft:commonBukkit")
+include("minecraft:folia")
+include("minecraft:paper")
+include("minecraft:velocity")
+
+dependencyResolutionManagement {
+//    versionCatalogs {
+//        create("libs") {
+//            from(files("gradle/libs.versions.toml"))
+//        }
+//    }
+}
