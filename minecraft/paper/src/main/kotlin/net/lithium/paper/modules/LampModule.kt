b7df07c0ce7ad291f2e3831dec4f96bcaa6e1fd8
kamiql <kilianaqua08@gmail.com>
2026-08-03 00:02:37 +0200

    fix: koin declaration for lamp api class fixed

--- minecraft/paper/src/main/kotlin/net/lithium/paper/modules/LampModule.kt
+++ minecraft/paper/src/main/kotlin/net/lithium/paper/modules/LampModule.kt
@@ -2,12 +2,14 @@ package net.lithium.paper.modules
 
 import net.lithium.paper.PaperMain
 import org.koin.dsl.module
+import revxrsal.commands.Lamp
 import revxrsal.commands.bukkit.BukkitLamp
+import revxrsal.commands.bukkit.actor.BukkitCommandActor
 
 val LampModule = module {
-    single {
+    single<Lamp<BukkitCommandActor>> {
         BukkitLamp.builder(get<PaperMain>()).accept {
 
-        }
+        }.build()
     }
 }
